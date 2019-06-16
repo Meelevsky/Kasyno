cf4468271c6edaa595b57e06336092a8c37f795d
Łukasz Sierminski <lukasz.sierminski@gmail.com>
2019-06-16 16:48:11 +0200

    Druga czesc zadania

---
+++ Zadanie1/IDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kasyno_1
+{
+    public interface IDataService
+    {
+        IEnumerable<Katalog> WszystkiePozycjeKatalogu();
+        IEnumerable<Katalog> PrzeszukajlKatalog(string nazwaGry);
+        void DodajZdarzenie(Zdarzenie zdarzenie);
+        IEnumerable<Zdarzenie> WyswietlPowiazaneZdarzenia();
+        IEnumerable<Gracz> WyswietlWszystkichGraczy();
+        IEnumerable<Gracz> ZnajdzGracza(string nazwisko);
+        
+    }
+}
